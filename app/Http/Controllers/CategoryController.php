000d56bef8e115edbd7b164498a87418e3ad860b
Anand MS <anandagireesh@gmail.com>
2025-04-12 14:50:34 +0400

    updated

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,51 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\Category\CreateCategory;
+use App\Interfaces\Interfaces\Category\CategoryInterface;
 use App\Models\Category;
 use App\Services\ApiResponseService;
 use Illuminate\Http\Request;
-use App\Repositories\Category\CategoryRepository;
 
 class CategoryController extends Controller
 {
     protected $apiResponseService;
     protected $categoryRepository;
+    protected $categoryInterface;
 
-    public function __construct(ApiResponseService $apiResponseService, CategoryRepository $categoryRepository)
+    public function __construct(ApiResponseService $apiResponseService, CategoryInterface $categoryInterface)
     {
         $this->apiResponseService = $apiResponseService;
-        $this->categoryRepository = $categoryRepository;
-    }
-    public function create(CreateCategory $request)
-    {
-        $category = $this->categoryRepository->storeCategory($request);
-        return $this->apiResponseService->success('Category created successfully', $category);
+        $this->categoryInterface = $categoryInterface;
     }
 
-    public function index()
+    public function create(Request $request)
     {
-        $filters = request()->all();
-        $categories = $this->categoryRepository->getCategories($filters);
-        return $this->apiResponseService->success(
-            'Categories fetched successfully',
-            $categories['data'],
-            200,
-            [
-                'pagination' => $categories['pagination']
-            ]
-        );
+        return $this->categoryInterface->storeCategory($request);
     }
 
-    public function update(CreateCategory $request, Category $category)
+    public function index(Request $request)
     {
-        $category = $this->categoryRepository->updateCategory($request, $category);
-        return $this->apiResponseService->success('Category updated successfully', $category);
+        return $this->categoryInterface->getCategories($request->all());
     }
 
-    public function destroy(Category $category)
+    public function update(Request $request, $category)
     {
-        $category = $this->categoryRepository->deleteCategory($category);
-        return $this->apiResponseService->success('Category deleted successfully', $category);
+        return $this->categoryInterface->updateCategory($request, $category);
+    }
+
+    public function destroy($category)
+    {
+        return $this->categoryInterface->deleteCategory($category);
     }
 }
